a3d7df146d45a18f63b1c4a3447f22b18bab8a4b
gustavo castellanos <gustavo.castellanos@grupodms.com>
2014-02-14 09:16:18 -0600

    Correr script de wsgi para hacer deployment

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -1,21 +1,24 @@
 import os, sys, site
 from os.path import abspath, dirname
 from sys import path
+from project.settings.base import VIRTUALENV, NOMBREDEUSUARIO
 
 try:
+
 	SITE_ROOT = dirname(dirname(abspath(__file__)))
 	path.append(SITE_ROOT)
 	
 	# Tell wsgi to add the Python site-packages to its path. 
-	site.addsitedir('/home/{{usuario}}/.virtualenvs/{{maquina_virtual_nombre}}/lib/python2.7/site-packages')
+	site.addsitedir('/home/'+ NOMBREDEUSUARIO +'/.virtualenvs/'+ VIRTUALENV +'/lib/python2.7/site-packages')
 	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.produccion")
-	activate_this = os.path.expanduser("~/.virtualenvs/{{maquina_virtual_nombre}}/bin/activate_this.py")
+	activate_this = os.path.expanduser("~/.virtualenvs/"+ VIRTUALENV +"/bin/activate_this.py")
 	execfile(activate_this, dict(__file__=activate_this))
 	
 	# Calculate the path based on the location of the WSGI script
-	project = '/home/{{usuario}}/webapps/{{nombre_de_la_aplicacion}}/project/project/'
+	project = '/home/'+ NOMBREDEUSUARIO +'/webapps/'+ VIRTUALENV +'/project/project/'
 	workspace = os.path.dirname(project)
 	sys.path.append(workspace)
+	
 except: 
 
 	pass
